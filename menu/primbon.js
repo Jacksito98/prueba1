2640261adbaa03f44d960b0a0ed30507da2b8e81
Jacksito98 <80754943+Jacksito98@users.noreply.github.com>
2021-03-28 07:54:27 -0400

    Add files via upload

---
+++ menu/primbon.js
@@ -0,0 +1,35 @@
+const a = '```'
+const primbon = (prefix, pushname2, groupName, user, name) => {
+return `
+
+╭─────≽「 *𝐌𝐄𝐍𝐔 ${name}* 」
+┴
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎:${a} *${pushname2}*
+┣⊱  ${a}𝐍𝐎𝐌𝐁𝐑𝐄 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎:${a} *${groupName}*
+┣⊱  ${a}𝐕𝐄𝐑𝐒𝐈𝐎𝐍:${a} *0.0.0*
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎:${a} *${user.length} User*
+┬
+╰────────────────────────
+
+╭─────≽「 *𝐏𝐑𝐈𝐌𝐁𝐎𝐍 𝐌𝐄𝐍𝐔* 」
+┴
+┠≽ *${prefix}apakah aku ganteng?*
+┠≽ *${prefix}kapankah aku menikah?*
+┠≽ *${prefix}bisakah aku memilikimu?*
+┠≽ *${prefix}rate reply something*
+┠≽ *${prefix}watak Farhan*
+┠≽ *${prefix}hobby Farhan*
+┠≽ *${prefix}gantengcek Farhan*
+┠≽ *${prefix}cantikcek Iriene*
+┠≽ *${prefix}persengay Topan*
+┠≽ *${prefix}pbucin Farhan*
+┠≽ *${prefix}mimpi Ular*
+┠≽ *${prefix}artinama Farhan*
+┠≽ *${prefix}pasangan Farhan/Iriene*
+┠≽ *${prefix}tanggaljadian 13/02/2021*
+┠≽ *${prefix}zodiak taurus*
+┬
+╰────────────────────────
+` 
+}
+exports.primbon = primbon
