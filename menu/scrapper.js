c07eb67c1ee263c60eaf08e364d7d4ea72392d57
Jacksito98 <80754943+Jacksito98@users.noreply.github.com>
2021-03-28 07:54:55 -0400

    Add files via upload

---
+++ menu/scrapper.js
@@ -0,0 +1,27 @@
+const a = '```'
+const scrapper = (prefix, pushname2, groupName, user, name) => {
+return `
+╭─────≽「 *𝐌𝐄𝐍𝐔 ${name}* 」
+┴
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎:${a} *${pushname2}*
+┣⊱  ${a}𝐍𝐎𝐌𝐁𝐑𝐄 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎:${a} *${groupName}*
+┣⊱  ${a}𝐕𝐄𝐑𝐒𝐈𝐎𝐍:${a} *0.0.0*
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎:${a} *${user.length} User*
+┬
+╰────────────────────────
+
+╭──≽「 *𝐇𝐈𝐒𝐓𝐎𝐑𝐈𝐀 𝐘 𝐒𝐂𝐑𝐀𝐏𝐏𝐄𝐑* 」
+┴
+┠≽ *${prefix}wattpad tere liye*
+┠≽ *${prefix}randomkpop*
+┠≽ *${prefix}cersex*
+┠≽ *${prefix}randombokep*
+┠≽ *${prefix}pornhub stepMoms*
+┠≽ *${prefix}xvideos japan*
+┠≽ *${prefix}xnxx japan*
+┠≽ *${prefix}nekopoi oni chichi*
+┬
+╰────────────────────────
+`
+}
+exports.scrapper = scrapper
