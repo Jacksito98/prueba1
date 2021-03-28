2cbac0d0a3f1945c54f705eb4dd3f61ad49b0468
Jacksito98 <80754943+Jacksito98@users.noreply.github.com>
2021-03-28 07:52:39 -0400

    Add files via upload

---
+++ menu/others.js
@@ -0,0 +1,42 @@
+const a = '```'
+const others = (prefix, pushname2, groupName, user, name) => {
+return `
+
+╭─────≽「 *𝐌𝐄𝐍𝐔 ${name}* 」
+┴
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎:${a} *${pushname2}*
+┣⊱  ${a}𝐍𝐎𝐌𝐁𝐑𝐄 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎:${a} *${groupName}*
+┣⊱  ${a}𝐕𝐄𝐑𝐒𝐈𝐎𝐍:${a} *0.0.0*
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎:${a} *${user.length} User*
+┬
+╰────────────────────────
+
+╭─────≽「 *𝐎𝐓𝐑𝐎 𝐌𝐄𝐍𝐔* 」
+┴
+┠≽ *${prefix}randomexo*
+┠≽ *${prefix}randombts*
+┠≽ *${prefix}blackpink*
+┠≽ *${prefix}anjing*
+┠≽ *${prefix}kucing*
+┠≽ *${prefix}testime*
+┠≽ *${prefix}quotes*
+┠≽ *${prefix}katabijak*
+┠≽ *${prefix}bucin*
+┠≽ *${prefix}bacotandilan*
+┠≽ *${prefix}pantun*
+┠≽ *${prefix}hekerbucin*
+┠≽ *${prefix}katailham*
+┬
+╰────────────────────────
+
+╭─────≽「 *𝐈𝐒𝐋𝐀𝐌𝐈𝐂 𝐌𝐄𝐍𝐔* 」
+┴
+┠≽ *${prefix}jadwalsholat Banyuwangi*
+┠≽ *${prefix}quran*
+┠≽ *${prefix}listsurah*
+┠≽ *${prefix}quransurah 1*
+┬
+╰────────────────────────
+`
+}
+exports.others = others
