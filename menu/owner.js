64af8afd095ee8b674797a85f53cc1d3e75a2351
Jacksito98 <80754943+Jacksito98@users.noreply.github.com>
2021-03-28 07:53:11 -0400

    Add files via upload

---
+++ menu/owner.js
@@ -0,0 +1,47 @@
+const a = '```'
+const ownerrr = (prefix, pushname2, groupName, user, name) => {
+return `
+
+╭─────≽「 *𝐌𝐄𝐍𝐔 ${name}* 」
+┴
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎:${a} *${pushname2}*
+┣⊱  ${a}𝐍𝐎𝐌𝐁𝐑𝐄 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎:${a} *${groupName}*
+┣⊱  ${a}𝐕𝐄𝐑𝐒𝐈𝐎𝐍:${a} *0.0.0*
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎:${a} *${user.length} User*
+┬
+╰────────────────────────
+
+╭────────≽「 *𝐒𝐎𝐋𝐎 𝐎𝐖𝐍𝐄𝐑* 」
+┴
+┠≽ *${prefix}addstatus*
+┠≽ *${prefix}addbadword*
+┠≽ *${prefix}dellbadword*
+┠≽ *${prefix}zalgo*
+┠≽ *${prefix}vapor*
+┠≽ *${prefix}pitch*
+┠≽ *${prefix}addpremium mentioned*
+┠≽ *${prefix}dellpremium mentioned*
+┠≽ *${prefix}setmemlimit*
+┠≽ *${prefix}setlimit*
+┠≽ *${prefix}setreply*
+┠≽ *${prefix}setprefix*
+┠≽ *${prefix}setnamebot*
+┠≽ *${prefix}setppbot*
+┠≽ *${prefix}setreplyimage*
+┠≽ *${prefix}sharelock*
+┠≽ *${prefix}chatlist*
+┠≽ *${prefix}bc*
+┠≽ *${prefix}bcgc*
+┠≽ *${prefix}ban*
+┠≽ *${prefix}unban*
+┠≽ *${prefix}block*
+┠≽ *${prefix}unblock*
+┠≽ *${prefix}clearall*
+┠≽ *${prefix}delete*
+┠≽ *${prefix}clone*
+┠≽ *${prefix}leave*
+┬
+╰────────────────────────
+`
+}
+exports.ownerrr = ownerrr
