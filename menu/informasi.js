95d634b8b9a57b93c99930e48a96e95552723883
Jacksito98 <80754943+Jacksito98@users.noreply.github.com>
2021-03-28 07:50:20 -0400

    Add files via upload

---
+++ menu/informasi.js
@@ -0,0 +1,41 @@
+const a = '```'
+const informasi = (prefix, pushname2, groupName, user, name) => {
+return `
+╭─────≽「 *𝐌𝐄𝐍𝐔 ${name}* 」
+┴
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎:${a} *${pushname2}*
+┣⊱  ${a}𝐍𝐎𝐌𝐁𝐑𝐄 𝐃𝐄𝐋 𝐆𝐑𝐔𝐏𝐎:${a} *${groupName}*
+┣⊱  ${a}𝐕𝐄𝐑𝐒𝐈𝐎𝐍:${a} *0.0.0*
+┣⊱  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎:${a} *${user.length} User*
+┬
+╰────────────────────────
+
+╭─────≽「 *𝐌𝐄𝐍𝐔 𝐃𝐄 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍* 」
+┴
+┠≽ *${prefix}fakta*
+┠≽ *${prefix}infogempa*
+┠≽ *${prefix}jarak Banyuwangi/Surabaya*
+┠≽ *${prefix}translate en/Apa kabar?*
+┠≽ *${prefix}bpfont Farhan*
+┠≽ *${prefix}textstyle FXC7*
+┠≽ *${prefix}jadwaltv antv*
+┠≽ *${prefix}jadwaltvnow*
+┠≽ *${prefix}lirik melukis senja*
+┠≽ *${prefix}chord Melukis senja*
+┠≽ *${prefix}wiki Adolf Hitler*
+┠≽ *${prefix}brainly pertanyaan*
+┠≽ *${prefix}resepmasakan bakwan*
+┠≽ *${prefix}map Banyuwangi*
+┠≽ *${prefix}film Fast and Farious*
+┠≽ *${prefix}pinterest gambar kucing*
+┠≽ *${prefix}infocuaca Banyuwangi*
+┠≽ *${prefix}jamdunia Banyuwangi*
+┠≽ *${prefix}infoalamat jalan Banyuwangi*
+┠≽ *${prefix}playstore WhatsApp*
+┠≽ *${prefix}moddroid lightroom*
+┠≽ *${prefix}happymod lightroom*
+┬
+╰───────────────────────────
+`
+}
+exports.informasi = informasi
