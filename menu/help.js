e1d40297911b385c78a967468332d55c88ed091a
Jacksito98 <80754943+Jacksito98@users.noreply.github.com>
2021-03-28 07:48:56 -0400

    Add files via upload

---
+++ menu/help.js
@@ -0,0 +1,408 @@
+// menu fitur bot
+const a = '```'
+const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam, tanggal, groupName, premi, Simihh, Welcomee, ModeAnime, Nsfww, BadWordd, AntiLinkk, AntiVirtexx) => { 
+	return `
+	
+${a}Follow My Instagram${a}
+https://www.instagram.com/jack_paymon/?hl=es
+
+
+
+${a}My Youtube${a}
+por ahora no
+
+${a}𝐏𝐀𝐑𝐀 𝐋𝐀𝐒 𝐂𝐀𝐑𝐀𝐂𝐓𝐄𝐑𝐈𝐒𝐓𝐈𝐂𝐀𝐒 𝐀𝐂𝐓𝐔𝐀𝐋𝐈𝐙𝐀𝐃𝐀𝐒 𝐏𝐎𝐑 𝐅𝐀𝐕𝐎𝐑! 𝐄𝐒𝐂𝐑𝐈𝐁𝐀${a} *${prefix}infobot*
+
+╭─────≽「 *𝐌𝐄𝐍𝐔#1 ${name}* 」
+┴
+┣⊱>  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎:${a} *${pushname2}*
+┣⊱>  ${a}𝐏𝐑𝐄𝐌𝐈𝐔𝐌:${a} ${premi}
+┣⊱>  ${a}𝐋𝐈𝐌𝐈𝐓𝐄𝐒:${a} *${500} perhari*
+│
+┣⊱>  ${a}GRUPO:${a} *${groupName}*
+┣⊱>  ${a}MODE WELCOME:${a} *${Welcomee}*
+┣⊱>  ${a}MODE SIMI:${a} *${Simihh}*
+┣⊱>  ${a}MODE ANIME:${a} *${ModeAnime}*
+┣⊱>  ${a}MODE NSFW:${a} *${Nsfww}*
+┣⊱>  ${a}MODE ANTI BADWORD:${a} *${BadWordd}*
+┣⊱>  ${a}MODE ANTI LINK:${a} *${AntiLinkk}*
+┣⊱>  ${a}MODE ANTI VIRTEX:${a} *${AntiVirtexx}*
+│
+┣⊱>  ${a}𝐇𝐎𝐑𝐀:${a} *${jam} WIB*
+┣⊱>  ${a}𝐅𝐄𝐂𝐇𝐀:${a} *${tanggal}*
+┣⊱>  ${a}𝐀𝐂𝐓𝐈𝐕𝐎:${a} ${kyun(uptime)}
+┣⊱>  ${a}𝐕𝐄𝐑𝐒𝐈𝐎𝐍:${a} *0.0.0*
+┣⊱>  ${a}𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎:${a} *${user.length} User*
+┬
+╰────────────────────────
+
+╭─────≽「 *𝐒𝐎𝐁𝐑𝐄 ${name}* 」
+┴
+┠─≽ *${prefix}mutualan*
+│ ${a}chats de amigos aleatorios${a}
+┠─≽ *${prefix}report lapor bug*
+│ ${a}para informar de errores al propietario${a}
+┠─≽ *${prefix}info*
+│ ${a}para mostrar la informacion del bot${a}
+┠─≽ *${prefix}donasi*
+│ ${a}si desea donar${a}
+┠─≽ *${prefix}owner*
+│ ${a}envia el numero del owner y nombre${a}
+┠─≽ *${prefix}speed*
+│ ${a}que tan rapido va el bot${a}
+┠─≽ *${prefix}daftar*
+│ ${a}para registrarte y poder usar el bot${a}
+┠─≽ *${prefix}limit*
+│ ${a}para verificar su limite${a}
+┠─≽ *${prefix}blocklist*
+│ ${a}para ver los contactos que el bot ha bloqueado${a}
+┠─≽ *${prefix}banlist*
+│ ${a}verificar los contactos prohibidos${a}
+┠─≽ *${prefix}premiumlist*
+│ ${a}muestra la lista de usuarios premium${a}
+┠─≽ *${prefix}bahasa*
+│ ${a}para la funcion de texto a voz${a}
+┠─≽ *${prefix}wame*
+│ ${a}cree su enlace de numero WA${a}
+┠─≽ *${prefix}cekpremium*
+│ ${a}verifique su título vencido${a}
+┠─≽ *${prefix}listbadword*
+│ ${a}lista de malas palabras${a}
+┬
+╰────────────────────────
+
+╭──≽「 *𝐂𝐀𝐑𝐀𝐂𝐓𝐄𝐑𝐈𝐒𝐓𝐈𝐂𝐀𝐒 𝐒𝐈𝐌𝐏𝐋𝐄𝐒 ${name}* 」
+┴
+┠─≽ *${prefix}menucreator*
+│ ${a}menu imágenes maker${a}
+┠─≽ *${prefix}menumedia*
+│ ${a}menu de descargas${a}
+┠─≽ *${prefix}menugrup*
+│ ${a}características especiales del grupo${a}
+┠─≽ *${prefix}menuothers*
+│ ${a}menu aleatorio${a}
+┠─≽ *${prefix}menuprimbon*
+│ ${a}menu de primbon coincidentes etc${a}
+┠─≽ *${prefix}menuinformasi*
+│ ${a}menu de informacion meteorologica${a}
+┠─≽ *${prefix}menufun*
+│ ${a}menu para jugar con amigos${a}
+┠─≽ *${prefix}menuscrapper*
+│ ${a}menu scrapper +18 uwu${a}
+┠─≽ *${prefix}menuencrypt*
+│ ${a}menu de cifrar y descifrar${a}
+┠─≽ *${prefix}menuspam*
+│ ${a}menu spam owner${a}
+┠─≽ *${prefix}menupremium*
+│ ${a}menu para premiuns${a}
+┠─≽ *${prefix}menuowner*
+│ ${a}menu para el owner${a}
+┬
+╰────────────────────────
+
+╭────≽「 *𝐒𝐔𝐏𝐏𝐎𝐑𝐓${name}* 」
+┴
+│➲ ${a}𝐉𝐚𝐜𝐤 𝐚𝐧𝐝𝐲 𝐬𝐨𝐥𝐞𝐫 𝐫𝐞𝐲𝐞𝐬${a}
+│➲ ${a}𝐜𝐫𝐞𝐚𝐝𝐨𝐫${a}
+│➲ ${a}𝐭𝐫𝐚𝐝𝐮𝐜𝐢𝐝𝐨${a}
+│➲ ${a}${a}
+│➲ ${a}${a}
+│➲ ${a}${a}
+│➲ ${a}${a}
+┬
+╰──────≽ *Created © 𝕵𝖆𝖈𝖐𝕽𝕯*`
+}
+
+exports.help = help
+
+// penghitung aktif bot
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+  return `*${pad(hours)} Jam ${pad(minutes)} Menit ${pad(seconds)} Detik*`
+}
+
+// info bot 
+const bottt = (prefix) => {
+return `
+${a}por ahora el bot solo se puede usar en grupos ${a} \n*por aquellos que hacen mucho spam en el DM*
+
+*NOTE:*
+si este bot esta apagado en el grupo el digale al owner que lo active, ojo que siempre vive durmiendo xD por cierto${prefix}bot activo
+
+*ponte de freco pa que tu vea como se te manda soporte en 10 minutos 🙂*
+
+- ${prefix}brainly
+- ${prefix}kalkulator
+- ${prefix}ytsearch
+- ${prefix}moddroid
+- ${prefix}happymod
+- ${prefix}playstore
+- ${prefix}tanggaljadian
+- ${prefix}stiker
+- ${prefix}resepmasakan
+- ${prefix}film
+- ${prefix}infocuaca
+- ${prefix}infogempa
+- ${prefix}tahta
+- ${prefix}igstalk
+- ${prefix}tiktokstalk
+- ${prefix}instastory
+- ${prefix}translate
+- ${prefix}quran
+- ${prefix}tafsirquran
+- ${prefix}lirik
+- ${prefix}chord
+- ${prefix}nulis
+- ${prefix}puisiimg
+- ${prefix}randomkpop
+- ${prefix}quotes
+- ${prefix}bucin
+- ${prefix}artinama
+- ${prefix}wattpad
+- ${prefix}jarak
+- ${prefix}infoalamat
+- ${prefix}mimpi
+- ${prefix}bacotandilan
+- ${prefix}instavid
+- ${prefix}instaimg
+
+*Apa Yang Terbaru??*
+
+~ random gambar KPOP ✓
+~ random gambar EXO ✓
+~ Fitur Anti virtex ✓
+~ Fitur Anti Link ✓
+~ Bacotan Dilan ✓
+~ wattpad searching ✓
+~ puisi dalam bentuk gambar ✓
+~ Mencari resep masakan ✓
+~ text to picture (bisa request warna) ✓
+~ Tiktok scrapper ✓
+~ instagram image downloader ✓ => command: ${prefix}instaimg link url
+~ instagram video downloader ✓ => command: ${prefix}instavid link url
+
+~ Resep Masakan ✓ => comand: ${prefix}resepmasakan rawon [ contoh]
+
+~ Anti Virtex on/off ✓ command: ${prefix}antivirtex on
+- *Gunakan Apabila Ada Yg Ngirim Virtex*
+
+~ Anti Virtex ✓ => command: ${prefix}antivirtexx
+- *el bot enviara mensajes de texto para que los miembros no experimenten lag*
+
+~ Xnxx scrapper ✓
+
+`
+}
+exports.bottt = bottt
+// donasi menu
+
+const donasi = (name) => { 
+	return `       
+╭─≽「 *𝐃𝐎𝐍𝐀 𝐏𝐀𝐑𝐀 𝐐𝐔𝐄 𝐄𝐋 𝐁𝐎𝐓 𝐒𝐄 𝐌𝐀𝐍𝐓𝐄𝐍𝐆𝐀 𝐀𝐂𝐓𝐈𝐕𝐎* 」
+┴
+│✓ *𝑷𝑨𝒀𝑷𝑨𝑳: https://www.paypal.me/jacksito23*│
+│✓ ${a}${a}
+│ *0.50 𝑼𝑺𝑫 𝒚 𝒑𝒖𝒆𝒅𝒆 𝒊𝒏𝒗𝒊𝒕𝒂𝒓 𝒂𝒍 𝒃𝒐𝒕 𝒂 𝒔𝒖 𝒈𝒓𝒖𝒑𝒐 𝒅𝒖𝒓𝒂𝒏𝒕𝒆 1 𝒎𝒆𝒔*
+│
+│✓ ${a}OVO:${a}
+│ 
+┬
+╰─────≽「 *𝕭𝖞 ${name}* 」
+
+𝐩𝐚𝐫𝐚 𝐥𝐚 𝐬𝐮𝐩𝐞𝐫𝐯𝐢𝐯𝐞𝐧𝐜𝐢𝐚 𝐝𝐞𝐥 𝐛𝐨𝐭 𝐝𝐞𝐛𝐢𝐝𝐨 𝐚 𝐥𝐚𝐬 𝐜𝐨𝐬𝐭𝐨𝐬𝐚𝐬 𝐜𝐮𝐨𝐭𝐚𝐬:'
+`
+}
+exports.donasi = donasi
+
+const listsurah = () => {
+return `
+1. Al-Fatihah الفاتحة
+2. Al-Baqarah البقرة
+3. Ali ‘Imran آل عمران
+4. An-Nisa’ النّساء
+5. Al-Ma’idah المآئدة
+6. Al-An’am الانعام
+7. Al-A’raf الأعراف
+8. Al-Anfal الأنفال
+9. At-Taubah التوبة
+10. Yunus ينوس
+11. Hud هود
+12. Yusuf يسوف
+13. Ar-Ra’d الرّعد
+14. Ibrahim إبراهيم
+15. Al-Hijr الحجر
+16. An-Nahl النّحل
+17. Al-Isra’ بني إسرائيل
+18. Al-Kahf الكهف
+19. Maryam مريم
+20. Ta Ha طه
+21. Al-Anbiya الأنبياء
+22. Al-Hajj الحجّ
+23. Al-Mu’minun المؤمنون
+24. An-Nur النّور
+25. Al-Furqan الفرقان
+26. Asy-Syu’ara’ الشّعراء
+27. An-Naml النّمل
+28. Al-Qasas القصص
+29. Al-‘Ankabut العنكبوت
+30. Ar-Rum الرّوم
+31. Luqman لقمان
+32. As-Sajdah السّجدة
+33. Al-Ahzab الْأحزاب
+34. Saba’ سبا
+35. Fatir فاطر
+36. Ya Sin يس
+37. As-Saffat الصّافات
+38. Sad ص
+39. Az-Zumar الزّمر
+40. Al-Mu’min المؤمن
+41. Fussilat فصّلت
+42. Asy-Syura الشّورى
+43. Az-Zukhruf الزّخرف
+44. Ad-Dukhan الدّخان
+45. Al-Jasiyah الجاثية
+46. Al-Ahqaf الَأحقاف
+47. Muhammad محمّد
+48. Al-Fath الفتح
+49. Al-Hujurat الحجرات
+50. Qaf ق
+51. Az-Zariyat الذّاريات
+52. At-Tur الطّور
+53. An-Najm النّجْم
+54. Al-Qamar القمر
+55. Ar-Rahman الرّحْمن
+56. Al-Waqi’ah الواقعه
+57. Al-Hadid الحديد
+58. Al-Mujadilah المجادلة
+59. Al-Hasyr الحشْر
+60. Al-Mumtahanah الممتحنة
+61. As-Saff الصّفّ
+62. Al-Jumu’ah الجمعة
+63. Al-Munafiqun المنافقون
+64. At-Tagabun التّغابن
+65. At-Talaq الطّلاق
+66. At-Tahrim التّحريم
+67. Al-Mulk الملك
+68. Al-Qalam القلم
+69. Al-Haqqah الحآقّة
+70. Al-Ma’arij المعارج
+71. Nuh نوح
+72. Al-Jinn الجنّ
+73. Al-Muzzammil المزمّل
+74. Al-Muddassir المدشّر
+75. Al-Qiyamah القيمة
+76. Al-Insan الْاٍنسان
+77. Al-Mursalat المرسلات
+78. An-Naba’ النّبا
+79. An-Nazi’at النّازعات
+80. ‘Abasa عبس
+81. At-Takwir التّكوير
+82. Al-Infitar الانفطار
+83. Al-Tatfif المطفّفين
+84. Al-Insyiqaq الانشقاق
+85. Al-Buruj البروج
+86. At-Tariq الطّارق
+87. Al-A’la الْأعلى
+88. Al-Gasyiyah الغاشية
+89. Al-Fajr الفجر
+90. Al-Balad البلد
+91. Asy-Syams الشّمس
+92. Al-Lail الّيل
+93. Ad-Duha الضحى
+94. Al-Insyirah الانشراح
+95. At-Tin التِّينِ
+96. Al-‘Alaq العَلَق
+97. Al-Qadr الْقَدْرِ
+98. Al-Bayyinah الْبَيِّنَةُ
+99. Az-Zalzalah الزلزلة
+100. Al-‘Adiyat العاديات
+101. Al-Qari’ah القارعة
+102. At-Takasur التكاثر
+103. Al-‘Asr العصر
+104. Al-Humazah الهُمَزة
+105. Al-Fil الْفِيلِ
+106. Quraisy قُرَيْشٍ
+107. Al-Ma’un الْمَاعُونَ
+108. Al-Kausar الكوثر
+109. Al-Kafirun الْكَافِرُونَ
+110. An-Nasr النصر
+111. Al-Lahab المسد
+112. Al-Ikhlas الإخلاص
+113. Al-Falaq الْفَلَقِ
+114. An-Nas نَاس
+ِ`
+}
+exports.listsurah = listsurah
+
+// bahasa list
+const bahasa = (prefix) => {
+return `
+List Bahasa Untuk Command *${prefix}tts*
+
+  af: Afrikaans
+  sq: Albanian
+  ar: Arabic
+  hy: Armenian
+  ca: Catalan
+  zh: Chinese
+  zh-cn: Chinese (Mandarin/China)
+  zh-tw: Chinese (Mandarin/Taiwan)
+  zh-yue: Chinese (Cantonese)
+  hr: Croatian
+  cs: Czech
+  da: Danish
+  nl: Dutch
+  en: English
+  en-au: English (Australia)
+  en-uk: English (United Kingdom)
+  en-us: English (United States)
+  eo: Esperanto
+  fi: Finnish
+  fr: French
+  de: German
+  el: Greek
+  ht: Haitian Creole
+  hi: Hindi
+  hu: Hungarian
+  is: Icelandic
+  id: Indonesian
+  it: Italian
+  ja: Japanese
+  ko: Korean
+  la: Latin
+  lv: Latvian
+  mk: Macedonian
+  no: Norwegian
+  pl: Polish
+  pt: Portuguese
+  pt-br: Portuguese (Brazil)
+  ro: Romanian
+  ru: Russian
+  sr: Serbian
+  sk: Slovak
+  es: Spanish
+  es-es: Spanish (Spain)
+  es-us: Spanish (United States)
+  sw: Swahili
+  sv: Swedish
+  ta: Tamil
+  th: Thai
+  tr: Turkish
+  vi: Vietnamese
+  cy: Welsh
+`
+}
+exports.bahasa = bahasa
+
+// Limit
+const limitcount = (limitCounts) => {
+        return`
+Limit Kamu: ${limitCounts}
+`
+}
+exports.limitcount = limitcount
